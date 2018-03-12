b36557a6b026f0640d88fe2a2604d37c60e74e8a
Вадим Шванов <adm.shwan@gmail.com>
2018-03-12 13:42:12 +0300

    Add project files.

---
+++ LinkedList/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Класс, описывающий элемент связного списка.
+    /// </summary>
+    /// <typeparam name="T"> Тип хранимых данных. </typeparam>
+    public class Item<T>
+    {
+        /// <summary>
+        /// Хранимые данные.
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Следующий элемент связного списка.
+        /// </summary>
+        public Item<T> Next { get; set; }
+
+        /// <summary>
+        /// Создание нового экземпляра связного списка.
+        /// </summary>
+        /// <param name="data"> Сохраняемые данные. </param>
+        public Item(T data)
+        {
+            // Не забываем проверять входные аргументы на null.
+            if(data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            Data = data;
+        }
+
+        /// <summary>
+        /// Приведение объекта к строке.
+        /// </summary>
+        /// <returns> Хранимые данные. </returns>
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем новый связный список.
+            var list = new LinkedList<int>();
+
+            // Добавляем элементы.
+            list.Add(1);
+            list.Add(5);
+            list.Add(17);
+            list.Add(42);
+            list.Add(-69);
+
+            // Выводим все элементы на консоль.
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            // Удаляем элемент.
+            list.Delete(17);
+
+            // Выводим все элементы еще раз.
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
